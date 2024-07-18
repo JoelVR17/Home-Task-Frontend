2a8c43a3467dfe701b6b16fc70d69100e73024ad
JoelVR17 <vermudo.com@gmail.com>
2024-07-18 10:04:48 -0600

    feat: Add Drag and Drop in tasks

--- src/components/Tasks/TaskCard.tsx
+++ src/components/Tasks/TaskCard.tsx
@@ -6,12 +6,17 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteTask } from "@/api/TaskAPI";
 import { toast } from "react-toastify";
+import { useDraggable } from "@dnd-kit/core";
 
 type TaskCardProps = {
   task: Task;
 };
 
 const TaskCard = ({ task }: TaskCardProps) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: task._id,
+  });
+
   const navigate = useNavigate();
   const params = useParams();
   const projectId = params.projectId!;
@@ -30,18 +35,30 @@ const TaskCard = ({ task }: TaskCardProps) => {
     },
   });
 
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+        padding: "1.25rem",
+        backgroundColor: "#fff",
+        width: "300px",
+        display: "flex",
+        borderWidth: "1px",
+        borderColor: "rgb(203 213 225 / var(--tw-border-opacity))",
+      }
+    : undefined;
+
   return (
     <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
-      <div className="min-w-0 flex flex-col gap-y-4">
-        <button
-          type="button"
-          className="text-xl font-bold text-slate-600 text-left"
-          onClick={() => {
-            navigate(location.pathname + `?viewTask=${task._id}`);
-          }}
-        >
+      <div
+        {...listeners}
+        {...attributes}
+        ref={setNodeRef}
+        style={style}
+        className="min-w-0 flex flex-col gap-y-4"
+      >
+        <p className="text-xl font-bold text-slate-600 text-left">
           {task.taskName}
-        </button>
+        </p>
         <p className="text-slate-500">{task.description}</p>
       </div>
       <div className="flex shrink-0  gap-x-6">
--- src/components/Tasks/TaskList.tsx
+++ src/components/Tasks/TaskList.tsx
@@ -1,6 +1,12 @@
-import { Task } from "@/types/index";
+import { Task, TaskStatus } from "@/types/index";
 import TaskCard from "./TaskCard";
 import { statusTranslations } from "@/locales/es";
+import DropTask from "./DropTask";
+import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import { updateStatus } from "@/api/TaskAPI";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
 
 type TaskListProps = {
   tasks: Task[];
@@ -27,34 +33,87 @@ const statusStyles: { [key: string]: string } = {
 };
 
 const TaskList = ({ tasks }: TaskListProps) => {
+  const queryClient = useQueryClient();
+  const params = useParams();
+
+  const projectId = params.projectId!;
+
+  const { mutate } = useMutation({
+    mutationFn: updateStatus,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ["project", projectId] });
+    },
+  });
+
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
     currentGroup = [...currentGroup, task];
     return { ...acc, [task.status]: currentGroup };
   }, initialStatusGroups);
+
+  const handleDragEnd = (e: DragEndEvent) => {
+    const { over, active } = e;
+
+    if (over && over.id) {
+      const taskId = active.id.toString();
+      const status = over.id as TaskStatus;
+
+      mutate({
+        projectId,
+        taskId,
+        status,
+      });
+
+      queryClient.setQueryData(["project", projectId], (oldData: any) => {
+        const updatedTask = oldData.tasks.map((task: Task) => {
+          if (task._id === taskId) {
+            return {
+              ...task,
+              status,
+            };
+          }
+
+          return task;
+        });
+
+        return {
+          ...oldData,
+          tasks: updatedTask,
+        };
+      });
+    }
+  };
+
   return (
     <>
       <h2 className="text-5xl font-black my-10">Tasks</h2>
 
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
-        {Object.entries(groupedTasks).map(([status, tasks]) => (
-          <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
-            <h3
-              className={`font-bold text-gray-600 capitalize text-xl text-center border border-slate-300 p-3 bg-white border-t-8 ${statusStyles[status]}`}
-            >
-              {statusTranslations[status]}
-            </h3>
-            <ul className="mt-5 space-y-5">
-              {tasks.length === 0 ? (
-                <li className="text-gray-500 text-center pt-3">
-                  There aren't tasks
-                </li>
-              ) : (
-                tasks.map((task) => <TaskCard key={task._id} task={task} />)
-              )}
-            </ul>
-          </div>
-        ))}
+        <DndContext onDragEnd={handleDragEnd}>
+          {Object.entries(groupedTasks).map(([status, tasks]) => (
+            <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+              <h3
+                className={`font-bold text-gray-600 capitalize text-xl text-center border border-slate-300 p-3 bg-white border-t-8 ${statusStyles[status]}`}
+              >
+                {statusTranslations[status]}
+              </h3>
+              <DropTask status={status} />
+              <ul className="mt-5 space-y-5">
+                {tasks.length === 0 ? (
+                  <li className="text-gray-500 text-center pt-3">
+                    There aren't tasks
+                  </li>
+                ) : (
+                  tasks.map((task) => <TaskCard key={task._id} task={task} />)
+                )}
+              </ul>
+            </div>
+          ))}
+        </DndContext>
       </div>
     </>
   );
